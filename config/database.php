055005a3b643707213fa74bdd4bde0321d25ca3e
Eka Yazılım ve Bilişim Sistemleri <132365560+ekayazilim@users.noreply.github.com>
2026-04-20 05:13:26 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ return [
     'host' => '127.0.0.1',
     'database' => 'eka_saas',
     'username' => 'root',
-    'password' => 'ServBay.dev',
+    'password' => '',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
